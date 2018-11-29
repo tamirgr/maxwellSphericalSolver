a9ca5e3e854d1a9671f0283ad3ae3acfba0c7d56
tamirgr <tamirgr@gmail.com>
2018-11-29 12:40:52 +0200

    Add files via upload

--- mySph2cart.m
+++ mySph2cart.m
@@ -4,7 +4,7 @@ function [X,Y,Z] = mySph2cart(rField,thField,phiField,th,phi)
     s2 = sin(phi);
     c1 = cos(th);
     c2 = cos(phi);
-    X = s1.*c2.*rField + c1.*c2.*thField + s2.*phiField;
-    Y = s1.*s2.*rField - c1.*s2.*thField + c2.*phiField;
+    X = s1.*c2.*rField + c1.*c2.*thField - s2.*phiField;
+    Y = s1.*s2.*rField + c1.*s2.*thField + c2.*phiField;
     Z = c1.*rField - s1.*thField ;
 end
