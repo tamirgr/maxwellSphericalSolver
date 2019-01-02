76c225dc4c484716535adf06dd7c88d0bea433c0
tamirgr <tamirgr@gmail.com>
2019-01-02 15:55:40 +0200

    Add files via upload

--- TEField.m
+++ TEField.m
@@ -1,20 +1,26 @@
-function [xlmTh,xlmPhi] = TEField(r,th,phi,sphr,epiNL,n,l,m)
+function [Th,Phi] = TEField(r,th,phi,sphr,epiNL,n,l,m)
+%% Init
 rho = sphr.k * sqrt(epiNL(l+1,n)/sphr.ep) .* r;
-
+ext = (r<=sphr.a);
 coeff = 1i*exp(1i.*m.*phi)./sqrt(l*(l+1)); % result of calculation with normalization coefficient for spherical bessel function
 plmcoeff = sqrt(4*pi/(2*l+1)*factorial(l+m)/factorial(l-m)); % normalization coefficient for associated legendre ploynomial
 % plmcoeff1 = sqrt(4*pi/(2*l+1)*factorial(l+m+1)/factorial(l-m-1)); % normalization coefficient for associated legendre ploynomial
+normce = normalizationCoeffCalc(sphr,rho,'E');
+%% Calculation
+Th = 1i.*m.*coeff./plmcoeff./sin(th).*legendrePlm(l,m,cos(th)).*normce;
 
-xlmTh = 1i.*m.*coeff./plmcoeff./sin(th).*legendrePlm(l,m,cos(th)).*normalizationCoeffCalc(sphr,rho,'E');
-
-xlmPhi = -1*coeff./plmcoeff.*(...
+Phi = -1*coeff./plmcoeff.*(...
     m.*cot(th).*legendrePlm(l,m,cos(th))...
     +legendrePlm(l,m+1,cos(th))...
-    ).*normalizationCoeffCalc(sphr,rho,'M');
+    ).*normce;
 
 xnl = sphr.k * sphr.a * sqrt(epiNL(l+1,n)/sphr.ep); % 
 coeffJH = SphericalBesselJ(l,xnl)/SphericalHankelH1(l,sphr.k*sphr.a);
 
-xlmTh  = xlmTh.*SphericalBesselJ(l,rho).*(r<=sphr.a)  + coeffJH*xlmTh.*SphericalHankelH1(l,rho).*(r>sphr.a);
-xlmPhi = xlmPhi.*SphericalBesselJ(l,rho).*(r<=sphr.a) + coeffJH*xlmPhi.*SphericalHankelH1(l,rho).*(r>sphr.a);
+spbj = SphericalBesselJ(l,rho);
+sph1 = SphericalHankelH1(l,rho);
+
+%% Result
+Th  = Th.*spbj.*ext  + coeffJH*Th.*sph1.*sphOut.*(~ext);
+Phi = Phi.*spbj.*ext + coeffJH*Phi.*sph1.*sphOut.*(~ext);
 end
--- TMField.m
+++ TMField.m
@@ -1,20 +1,14 @@
-function [xlmR,xlmTh,xlmPhi] = TMField(r,th,phi,sphr,epiNL,n,l,m)
-rho = sphr.k * sqrt(epiNL(l+1,n)/sphr.ep) .* r;
-
-coeff = 1i*exp(1i.*m.*phi)./sqrt(l*(l+1)); % result of calculation with normalization coefficient for spherical bessel function
-plmcoeff = sqrt(4*pi/(2*l+1)*factorial(l+m)/factorial(l-m)); % normalization coefficient for associated legendre ploynomial
-% plmcoeff1 = sqrt(4*pi/(2*l+1)*factorial(l+m+1)/factorial(l-m-1)); % normalization coefficient for associated legendre ploynomial
-
-xlmTh = 1i.*m.*coeff./plmcoeff./sin(th).*legendrePlm(l,m,cos(th)).*normalizationCoeffCalc(sphr,rho,'E');
-
-xlmPhi = -1*coeff./plmcoeff.*(...
-    m.*cot(th).*legendrePlm(l,m,cos(th))...
-    +legendrePlm(l,m+1,cos(th))...
-    ).*normalizationCoeffCalc(sphr,rho,'E');
+function [R,Th,Phi] = TMField(r,th,phi,sphr,epiNL,n,l,m)
+%% Init
+ext = (r<=sphr.a);
 
 xnl = sphr.k * sphr.a * sqrt(epiNL(l+1,n)/sphr.ep); % 
 coeffJH = SphericalBesselJ(l,xnl)/SphericalHankelH1(l,sphr.k*sphr.a);
-
-xlmTh  = xlmTh.*SphericalBesselJ(l,rho).*(r<=sphr.a)  + coeffJH*xlmTh.*SphericalHankelH1(l,rho).*(r>sphr.a);
-xlmPhi = xlmPhi.*SphericalBesselJ(l,rho).*(r<=sphr.a) + coeffJH*xlmPhi.*SphericalHankelH1(l,rho).*(r>sphr.a);
+%% Calculation
+[resRIn,resThIn,resPhiIn] = curlFXlmPoint2(r,th,phi,epiNL,sphr,n,l,m);
+[resROut,resThOut,resPhiOut] = curlFXlmPointHenkel2(r,th,phi,epiNL,sphr,n,l,m);    
+%% Result
+R   = 1i/sphr.k*(ext.*resRIn/epiNL(l,n)*sphr.ep   + coeffJH*(~ext).*resROut);
+Th  = 1i/sphr.k*(ext.*resThIn/epiNL(l,n)*sphr.ep  + coeffJH*(~ext).*resThOut);
+Phi = 1i/sphr.k*(ext.*resPhiIn/epiNL(l,n)*sphr.ep + coeffJH*(~ext).*resPhiOut);
 end
